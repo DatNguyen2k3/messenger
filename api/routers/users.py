c19b422aed61db314eaeec7083604b33148ef380
Dat Nguyen <datntd@otani-trading.com>
2023-05-05 09:11:14 +0700

    add doc for utils and api

--- api/routers/users.py
+++ api/routers/users.py
@@ -11,6 +11,10 @@ router = APIRouter()
 
 
 class User(BaseModel):
+    """
+    User model
+    """
+
     email: str
     username: str
     # avater_img: bytes
@@ -31,16 +35,13 @@ def get_all_users():
     return users
 
 
-
 # @router.post('/api/test_upload_file')
 # async def create_upload_file(file: UploadFile = File(...)):
 #     img_url = await saveAvartarImgToStatic(file, "test")
- 
+
 #     return {
 #         'url': img_url
 #     }
-
-
 
 
 # @router.post("/api/users")
@@ -51,7 +52,7 @@ def get_all_users():
 #     user = Users.create(**payload)
 #     user.avatar_img_url = await saveAvartarImgToStatic(avartar_img_file, user.username)
 #     user.save()
-    
+
 #     user = model_to_dict(user)
 #     return user
 
@@ -78,44 +79,49 @@ def get_all_users():
 #     """Delete user"""
 #     user = Users.get_by_id(id)
 #     user.delete_instance()
-    
+
 #     user = model_to_dict(user)
 #     return user
-    
 
 
 @router.post("/api/users/register")
-async def register(payload_: User = Depends(), avartar_img_file: UploadFile = File(...)):
-    """Register user"""
+async def register(
+    payload_: User = Depends(), avartar_img_file: UploadFile = File(...)
+):
+    """
+    Register user
+    if register success, return user info
+    if register fail, return error message
+    """
     payload = payload_.dict()
-    
+
     # validate email
     if not validate_email(payload["email"]):
-        return { "error": "Email is not valid"}
-    
+        return {"error": "Email is not valid"}
+
     # validate username
     if not valid_username(payload["username"]):
         return {"error": "Username is not valid"}
-    
+
     # check if username or email already exists
     if is_username_exists(payload["username"]) or is_email_exists(payload["email"]):
-        return { "error": "Username or email already exists"}
-    
+        return {"error": "Username or email already exists"}
+
     user = await create_user(payload, avartar_img_file)
     return user
 
 
 @router.get("/api/users/login")
 def login(username: str) -> dict:
-    '''
-        Login user
-        If login success, return user info
-        If login fail, return error message
-    '''
+    """
+    Login user
+    If login success, return user info
+    If login fail, return error message
+    """
     if not valid_username(username):
         return {"error": "Username is not valid"}
     if not is_username_exists(username):
         return {"error": "Username does not exist"}
-    
+
     user = get_user_by_username(username)
     return user
--- api/utils/users.py
+++ api/utils/users.py
@@ -7,27 +7,33 @@ USERNAME_CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234
 
 
 def convertFileName(file, newFileName):
+    """
+    Convert file name to new file name
+    """
     oldFileName = file.filename
     tail_index = oldFileName.rfind(".")
     return newFileName + oldFileName[tail_index:]
 
 
 async def saveAvartarImgToStatic(file, fileName):
+    """
+    Save avatar image to static folder
+    """
     file.filename = convertFileName(file, fileName)
     contents = await file.read()
-    
- 
-    #save the file
+    # save the file
     with open(f"{AVATAR_IMGS_DIR}{file.filename}", "wb") as f:
         f.write(contents)
- 
     return DOMAIN + AVATAR_IMGS_DIR + file.filename
 
 
 def valid_username(username):
+    """
+    Check if username is valid
+    """
     if username == "":
         return False
-    
+
     for char in username:
         if char not in USERNAME_CHARACTERS:
             return False
@@ -36,32 +42,39 @@ def valid_username(username):
 
 
 def is_username_exists(username):
+    """
+    Check if username exists
+    """
     if Users.select().where(Users.username == username).exists():
         return True
     return False
 
 
 def is_email_exists(email):
+    """
+    Check if email exists
+    """
     if Users.select().where(Users.email == email).exists():
         return True
     return False
 
 
 async def create_user(payload, avartar_img_file):
-    ''' 
-        Create user and return user info
-    '''
+    """
+    Create user and return user info
+    """
     user = Users.create(**payload)
     user.avatar_img_url = await saveAvartarImgToStatic(avartar_img_file, user.username)
     user.save()
-    
+
     user = model_to_dict(user)
-    return user          
+    return user
+
 
 def get_user_by_username(username: str) -> dict:
-    '''
-        Get user by username
-    '''
+    """
+    Get user by username
+    """
     user = Users.get_or_none(Users.username == username)
     user = model_to_dict(user)
     return user
