3f65cd14148efa0b30efdbabfe6d1b39f37cae69
Dat Nguyen <datntd@otani-trading.com>
2023-05-08 11:54:15 +0700

    create conversation api

--- api/models/conversations.py
+++ api/models/conversations.py
@@ -4,26 +4,54 @@ import uuid
 import datetime
 from .users import Users
 from playhouse.postgres_ext import ArrayField
+from utils.conversations import validate_members
+from playhouse.shortcuts import model_to_dict
 
-
-def members_validator(value):
-    for member in value:
-        if not Users.select().where(Users.id == member).exists():
-            raise ValueError('Member does not exist')
-            
-            
+               
+         
 class Conversations(PeeWeeBaseModel):
-    CONVERSATION_TYPES = (
+    '''
+    Conversations model
+    '''
+    CONVERSATION_TYPES = [
         ('Normal', 'Normal'),
         ('Group', 'Group')
-    )       
+    ]
             
     id = p.UUIDField(primary_key=True, default=uuid.uuid4, unique=True)
     created_by = p.ForeignKeyField(Users, backref='created_user')
     created_at = p.DateTimeField(default=datetime.datetime.now)
     modified_by = p.ForeignKeyField(Users, backref='modified_user')    
     modified_at = p.DateTimeField(default=datetime.datetime.now)
-    type = p.CharField(choices=CONVERSATION_TYPES)
+    type = p.CharField(choices=CONVERSATION_TYPES, default='Normal')
     members = ArrayField(p.TextField, default=[])
-
+    
+    @classmethod
+    def is_valid_conversation_type(cls, conversation_type):
+        '''
+        Check if conversation type is valid
+        '''
+        return conversation_type in [type[0] for type in cls.CONVERSATION_TYPES]
+    
+    
+    @classmethod
+    def create_conversation(cls, payload: dict) -> dict:
+        '''
+        Create a new conversation
+        '''
+        # Check if conversation type is valid
+        if not Conversations.is_valid_conversation_type(payload['type']):
+            return {'error': 'Invalid conversation type'}
+        
+        # Check if members are valid
+        try :
+            payload['members'] = validate_members(payload['members'])
+        except ValueError as value_error:
+            return {'error': str(value_error)}
+        
+        # Create conversation
+        conversation = Conversations.create(**payload)
+        conversation_dict = model_to_dict(conversation)
+        
+        return conversation_dict
             
--- api/routers/conversations.py
+++ api/routers/conversations.py
@@ -1,37 +1,39 @@
 from fastapi import APIRouter
 from playhouse.shortcuts import model_to_dict
-from models.conversations import Conversations, members_validator
-from pydantic import BaseModel
+from models.conversations import Conversations
+from schemas.conversation import Conversation
+from models.users import Users
+from utils.conversations import validate_members
 from models import psql_db
 
 
 router = APIRouter()
 db = psql_db
-
-
-class Conversation(BaseModel):
-    created_by: str
-    modified_by: str
-    type: str
-    members: list
     
     
 @router.post("/api/conversations/create_table")
 def create_conversations_table():
-    db.connect()
+    '''Create conversations table'''
     db.create_tables([Conversations])
-    db.close()
     
 
 @router.post("/api/conversations")
 def create_conversation(payload_: Conversation):
     """Create a new conversation"""
     payload = payload_.dict()
-    members_validator(payload['members'])
-    conversation = Conversations.create(**payload)
     
+    # Check if conversation type is valid
+    if not Conversations.is_valid_conversation_type(payload['type']):
+        return {'error': 'Invalid conversation type'}
+    
+    # Check if members are valid
+    try :
+        payload['members'] = validate_members(payload['members'])
+    except ValueError as value_error:
+        return {'error': str(value_error)}
+    
+    # Create conversation
+    conversation = Conversations.create(**payload)
     conversation_dict = model_to_dict(conversation)
-    return {
-        "input": payload,
-        "new_conversation": conversation_dict
-    }
+    
+    return conversation_dict
