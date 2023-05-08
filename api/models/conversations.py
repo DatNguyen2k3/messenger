d46ed101c91bed266004458e65695b5133d7a911
Dat Nguyen <datntd@otani-trading.com>
2023-05-08 13:02:54 +0700

    code create conversation api

--- api/models/conversations.py
+++ api/models/conversations.py
@@ -4,13 +4,17 @@ import uuid
 import datetime
 from .users import Users
 from playhouse.postgres_ext import ArrayField
-     
-            
+from utils.conversations import validate_members
+from playhouse.shortcuts import model_to_dict
+
 class Conversations(PeeWeeBaseModel):
-    CONVERSATION_TYPES = (
+    '''
+    Conversations model
+    '''
+    CONVERSATION_TYPES = [
         ('Normal', 'Normal'),
         ('Group', 'Group')
-    )       
+    ]
             
     id = p.UUIDField(primary_key=True, default=uuid.uuid4, unique=True)
     created_by = p.ForeignKeyField(Users, backref='created_user')
@@ -19,5 +23,33 @@ class Conversations(PeeWeeBaseModel):
     modified_at = p.DateTimeField(default=datetime.datetime.now)
     type = p.CharField(choices=CONVERSATION_TYPES, default='Normal')
     members = ArrayField(p.TextField, default=[])
-
+    
+    @classmethod
+    def is_valid_conversation_type(cls, conversation_type):
+        '''
+        Check if conversation type is valid
+        '''
+        return conversation_type in [type[0] for type in cls.CONVERSATION_TYPES]
+    
+    
+    @classmethod
+    def create_conversation(cls, payload: dict) -> dict:
+        '''
+        Create a new conversation
+        '''
+        # Check if conversation type is valid
+        if not Conversations.is_valid_conversation_type(payload['type']):
+            return {'error': 'Invalid conversation type'}
+        
+        # Check if members are valid
+        try :
+            payload['members'] = validate_members(payload['members'])
+        except ValueError as value_error:
+            return {'error': str(value_error)}
+        
+        # Create conversation
+        conversation = Conversations.create(**payload)
+        conversation_dict = model_to_dict(conversation)
+        
+        return conversation_dict
             
--- api/routers/conversations.py
+++ api/routers/conversations.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from playhouse.shortcuts import model_to_dict
-from models.conversations import Conversations, members_validator
-from pydantic import BaseModel
+from models.conversations import Conversations
+from schemas.conversation import Conversation
+from models.users import Users
+from utils.conversations import validate_members
 from models import psql_db
 
 
@@ -11,7 +13,27 @@ db = psql_db
     
 @router.post("/api/conversations/create_table")
 def create_conversations_table():
-    '''
-    Create conversations table
-    '''
+    '''Create conversations table'''
     db.create_tables([Conversations])
+    
+
+@router.post("/api/conversations")
+def create_conversation(payload_: Conversation):
+    """Create a new conversation"""
+    payload = payload_.dict()
+    
+    # Check if conversation type is valid
+    if not Conversations.is_valid_conversation_type(payload['type']):
+        return {'error': 'Invalid conversation type'}
+    
+    # Check if members are valid
+    try :
+        payload['members'] = validate_members(payload['members'])
+    except ValueError as value_error:
+        return {'error': str(value_error)}
+    
+    # Create conversation
+    conversation = Conversations.create(**payload)
+    conversation_dict = model_to_dict(conversation)
+    
+    return conversation_dict
