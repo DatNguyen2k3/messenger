e47bba22d3c2ffbc6f29494069f1da8bfe9199c8
Dat Nguyen <datntd@otani-trading.com>
2023-05-08 13:23:17 +0700

    Clean create conversation api

--- api/routers/conversations.py
+++ api/routers/conversations.py
@@ -22,18 +22,5 @@ def create_conversation(payload_: Conversation):
     """Create a new conversation"""
     payload = payload_.dict()
     
-    # Check if conversation type is valid
-    if not Conversations.is_valid_conversation_type(payload['type']):
-        return {'error': 'Invalid conversation type'}
-    
-    # Check if members are valid
-    try :
-        payload['members'] = validate_members(payload['members'])
-    except ValueError as value_error:
-        return {'error': str(value_error)}
-    
-    # Create conversation
-    conversation = Conversations.create(**payload)
-    conversation_dict = model_to_dict(conversation)
-    
-    return conversation_dict
+    conversation = Conversations.create_conversation(payload)
+    return conversation
