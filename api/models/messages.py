10e041455fb3a17f4f86cc7e3b135c3cf1218c8d
Dat Nguyen <datntd@otani-trading.com>
2023-05-09 14:59:48 +0700

    get latest message and get message api

--- api/models/messages.py
+++ api/models/messages.py
@@ -6,6 +6,7 @@ from .users import Users
 from .conversations import Conversations
 from schemas.message import Message   
 from playhouse.shortcuts import model_to_dict
+from typing import List, Optional
             
 class Messages(PeeWeeBaseModel):
     id = p.UUIDField(primary_key=True, default=uuid.uuid4, unique=True)
@@ -22,10 +23,38 @@ class Messages(PeeWeeBaseModel):
         '''Create message'''
         payload = payload_.dict()
         
+        from_user = Users.get_by_id(payload['from_user'])
         to_conversation = Conversations.get_by_id(payload['to_conversation'])
-        if payload['from_user'] not in to_conversation.members:
+        if str(payload['from_user']) not in to_conversation.members:
             raise ValueError("User is not a member of the conversation")
         
+        to_conversation.modified_at = datetime.datetime.now()
+        to_conversation.modified_by = from_user
+        to_conversation.save()
+        
         message = cls.create(**payload)
+        message_dict = model_to_dict(message)
+        return message_dict
+    
+    @classmethod
+    def get_messages(cls, conversation_id: uuid.UUID) -> List[dict]:
+        '''
+        Get latest messages
+        '''
+        
+        messages = Messages.select().where(Messages.to_conversation == conversation_id).order_by(Messages.created_at.desc())
+        
+        messages_dict = [model_to_dict(message) for message in messages]
+        return messages_dict
+    
+    
+    @classmethod
+    def get_latest_message(cls, conversation_id: uuid.UUID) -> dict:
+        '''
+        Get latest message
+        '''
+        
+        message = Messages.select().where(Messages.to_conversation == conversation_id).order_by(Messages.created_at.desc()).first()
+        
         message_dict = model_to_dict(message)
         return message_dict
--- api/routers/messages.py
+++ api/routers/messages.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 from models.messages import Messages
 from schemas.message import Message
 from models import psql_db
+import uuid
+from typing import List
 
 
 router = APIRouter()
@@ -23,3 +25,26 @@ def send_message(message: Message) -> dict:
         return {"error": str(e)}
     
     return message_dict
+
+@router.get("/api/messages")
+def get_messages(conversation_id: uuid.UUID) -> List[dict]:
+    '''Get messages'''
+    try:
+        messages = Messages.get_messages(conversation_id)
+    except Exception as e:
+        return {"error": str(e)}
+    
+    return messages
+
+
+@router.get("/api/messages/latest")
+def get_latest_messages(conversation_id: uuid.UUID) -> dict:
+    '''Get latest messages'''
+    try:
+        message = Messages.get_latest_message(conversation_id)
+    except Exception as e:
+        return {"error": str(e)}
+    
+    return message
+
+
