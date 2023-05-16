4d32af7ceeacb9499c3e4dd515a29f1768d40c70
Dat Nguyen <datntd@otani-trading.com>
2023-05-16 08:52:07 +0700

    add cors in backend

--- api/main.py
+++ api/main.py
@@ -2,12 +2,26 @@ from fastapi import FastAPI
 from routers import users, groups, messages, conversations
 from models import create_db
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 router = FastAPI(csrf=True)
 router.mount("/static", StaticFiles(directory="static"), name="static")
 db = create_db()
 
 db.connect()
+
+origins = [
+    "http://localhost:3000",  
+    "http://localhost:8000"
+]
+
+router.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @router.get("/")
 def root_access():
