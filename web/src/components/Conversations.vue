bd28bf8afc499a7fa3c4ff8ba14ba57dbf60ea63
Dat Nguyen <datntd@otani-trading.com>
2023-05-19 08:54:43 +0700

    code authentication

---
+++ web/src/components/Conversations.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+
+import { useRouter } from 'vue-router';
+
+export default {
+  name: 'conversations',
+}
+</script>
+import { useRouter } from 'vue-router';
+
+
+
+<style scoped>  
+
+
+
+</style>
